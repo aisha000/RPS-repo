96b6993586eb936f9039a2e13f9a0ee39a1d8f8f
aisha <68122062+aisha000@users.noreply.github.com>
2023-09-30 17:48:28 +0300

    update

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-function getRandomNumber(min, max){
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function game(){
+    for(let i = 0; i<5 ; i++){
+        function getComputerChoice(){
+        let computer =Math.floor(Math.random() * 3 + 1);
+        if(computer===1){
+            computer="rock";
+            return computer;
+        }
+    
+        else if(computer===2){
+            computer="paper";
+            return computer;
+        }
+        
+        else if(computer===3){
+            computer="scissor";
+            return computer;
+        }
+    }
+    let player = prompt("rock / paper / scissor ?").toLowerCase();
+    let computer = getComputerChoice();
+    
+    function round(){
+        let result = computer ===  player ? "tie game!"
+        : computer==="rock" && player==="scissor"? "computer wins"
+        : computer==="paper" && player==="rock"? "computer wins"
+        : computer==="scissor" && player==="paper"? "computer wins"
+        : "player wins!";
+        return result
+    }
+    
+    console.log(`player chose ${player}`)
+    console.log(`computer chose ${computer}`)
+    console.log(round())
+    }
 }
-let randomNumber = getRandomNumber(1, 3)
-
-alert(randomNumber)
+console.log(game())
